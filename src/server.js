574dd31027219f8d42ba20f0771bae5724742f00
KJH <stonehead334@gmail.com>
2022-03-31 11:20:16 +0900

    GET Request

--- src/server.js
+++ src/server.js
@@ -4,8 +4,16 @@ import express from "express";
 //1
 const app = express();
 
+//configure application
+//function ()를 꼭 보내야 한다
+//get은 브라우저가 유저한테 보내주는 request다
+app.get("/", () => console.log("Somebody"));
+
 const PORT = 4000;
-const handleListening = () => console.log(`✅Server listening on port http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
 //2 app을 listen to request server
 app.listen(PORT, handleListening);
+
+//GET Method
