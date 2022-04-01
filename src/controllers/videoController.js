1070c3d6b7d9d4ad39f6d68be0f208f240b1d3f5
KJH <stonehead334@gmail.com>
2022-04-01 14:03:51 +0900

    Use mixins for video section

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2m ago",
+      views: 87,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2m ago",
+      views: 87,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2m ago",
+      views: 87,
+      id: 1,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
@@ -12,3 +37,5 @@ export const upload = (req, res) => res.render("upload");
 export const writeComment = (req, res) => res.render("Write Comment");
 
 //변수를 제공하는 방법은 render에 파일명을 쓰고 변수를 쓴다. 원하는만큼 넣을 수 있다
+//mixins은 데이터를 받을 수 있는 일종의 미리 만들어진 HTML block 이라 볼 수 있다
+//
