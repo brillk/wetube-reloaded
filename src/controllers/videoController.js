ecfe4f99b2c25597e3127405056d1828fa90a661
KJH <stonehead334@gmail.com>
2022-04-01 13:32:44 +0900

    Templates-itearation / Using Array

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,7 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" }); 
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const removeVideo = (req, res) => res.render("remove");
@@ -8,4 +11,4 @@ export const upload = (req, res) => res.render("upload");
 
 export const writeComment = (req, res) => res.render("Write Comment");
 
-//변수를 제공하는 방법은 render에 파일명을 쓰고 변수를 쓴다. 원하는만큼 넣을 수 있다 
+//변수를 제공하는 방법은 render에 파일명을 쓰고 변수를 쓴다. 원하는만큼 넣을 수 있다
