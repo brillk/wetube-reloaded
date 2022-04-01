9cb56ca531c567454e962970ff9ef55ed7befb60
KJH <stonehead334@gmail.com>
2022-04-01 11:23:40 +0900

    Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,11 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" }); 
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const removeVideo = (req, res) => res.render("remove");
 
 export const search = (req, res) => res.render("search");
 export const upload = (req, res) => res.render("upload");
 
 export const writeComment = (req, res) => res.render("Write Comment");
+
+//변수를 제공하는 방법은 render에 파일명을 쓰고 변수를 쓴다. 원하는만큼 넣을 수 있다 
