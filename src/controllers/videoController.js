eef045f5f9f8967d7525c09dac0bd47d27c560b4
KJH <stonehead334@gmail.com>
2022-04-04 17:18:07 +0900

    Edit Video part 2, fixed # thing

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,19 +45,24 @@ export const watch = async (req, res) => {
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  //이제 아이디를 읽을 수 있으니 출력해보자
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
-    //만약 동영상이 없고 null상태라면 404.pug를 보낸다
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-  [id - 1].title = title; //제목 수정하기 -> 이건 진짜로 된건 아님 구동만 할뿐
-  return res.redirect(`//${id}`);
+  const { title, hashtags, description } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags.split(",").map(word => word.startsWith('#') ? word :`#${word}`);
+  await video.save();
+  return res.redirect(`/videos/${id}`);
 };
 
 //more practice
