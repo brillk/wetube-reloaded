11003bf8552c80309b4c566dbf5e1614d420f521
KJH <stonehead334@gmail.com>
2022-04-05 18:01:01 +0900

    Create Account part-2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,17 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {
-  console.log(req.body);
-  res.end();
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password, //하지만 db를 열어보면 비밀번호가 나온다 이걸 보안화해보자
+    location,
+  });
+  return res.redirect("/login");
+  //만든 정보를 로그인 페이지로 보내자
 };
 export const edit = (req, res) => res.send("Edit User");
 export const removeUser = (req, res) => res.send("Remove User");
