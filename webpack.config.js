afcb44d5e9dd0f39814c37aacd9cb4ad9484dffb
KJH <stonehead334@gmail.com>
2022-04-12 15:51:44 +0900

    MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/front-end/js/main.js", //내가 변경하고자 하는 파일
   mode: "development",
+  plugins: [new MiniCssExtractPlugin({ filename: "css/style.css" })],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -20,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -41,4 +43,9 @@ style-loader가 위 과정으로 변환시킨 css 코드를 DOM 내부에 적용
 4. 변환된 코드가 output에서 설정된 파일 경로에 설정된 파일명으로 저장된다
 
 5. 저장된 변환 js 코드를 pug 파일에 적용시키기 위해 'script(src="/static/js/main.js")' 코드를 통해 긁어와 적용시킨다
+
+MiniCssExtractPlugin
+이 플러그인은 CSS를 별도의 파일로 추출합니다. 
+CSS가 포함된 JS 파일별로 CSS 파일을 생성합니다. 
+mini-css-extract-plugin을 css-loader와 결합하는 것이 좋습니다.
 */
