fada6a271076bd3a7036d52ab8a595ba0804323f
KJH <stonehead334@gmail.com>
2022-04-12 14:25:00 +0900

    Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+const path = require("path");
+
 module.exports = {
-  entry: "./src/client/js/main.js", //내가 변경하고자 하는 파일
+  entry: "./src/front-end/js/main.js", //내가 변경하고자 하는 파일
+  mode: "development",
   output: {
     filename: "main.js",
-    path: "./assets/js",
+    path: path.resolve(__dirname, "assets", "js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
