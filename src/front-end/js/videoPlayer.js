6f047cec138130e70a0a22ed7fe033bff05a140d
KJH <stonehead334@gmail.com>
2022-04-13 10:00:01 +0900

    Volume

--- src/front-end/js/videoPlayer.js
+++ src/front-end/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = e => {
   //if the video playing, pause it
   if (video.paused) {
@@ -14,15 +17,33 @@ const handlePlayClick = e => {
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMute = e => {
+const handleMuteClick = e => {
   if (video.muted) {
     video.muted = false;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = event => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = Number(value); //볼륨을 음소거전으로 바꿔준다
+  video.volume = value;
+
+  if (volumeValue === 0) {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  }
 };
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
