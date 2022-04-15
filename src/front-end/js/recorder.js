95f913a02d3b48bcd7392591f3ff4550cdc13986
KJH <stonehead334@gmail.com>
2022-04-15 10:58:46 +0900

    Recording Video Part-1

--- src/front-end/js/recorder.js
+++ src/front-end/js/recorder.js
@@ -2,29 +2,31 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+
+const handleDownload = () => {};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording"; //녹화를 껏다 켰다, 이벤트 리스너를 상호작용
+  startBtn.innerText = "Download Recording"; //녹화를 껏다 켰다, 이벤트 리스너를 상호작용
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
 };
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
-  recorder.ondataavailable = e => {
-    console.log("recording done");
-    console.log(e);
-    console.log(e.data);
+
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = event => {
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
-  console.log(recorder);
   recorder.start();
-  console.log(recorder);
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
 };
 
 const init = async () => {
